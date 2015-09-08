6449064592cd2c5b9171c658789dd6be03f6d127
bangwu <bwu@thoughtworks.com>
2015-09-08 19:16:58 +0800

    use the angular way

--- src/main/webapp/scripts/jquery-way.js
+++ src/main/webapp/scripts/jquery-way.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var nameEle = $("#name");
     var nameVal = $("#nameValue");
 
-    nameEle[0].addEventListener('blur', nameChange);
+    nameEle[0].addEventListener('keyup', nameChange);
 
     function nameChange() {
         nameVal.html(this.value)
