3f8339f3afc0232419a7a5dfa6769f4e0ecbae34
bangwu <bwu@thoughtworks.com>
2015-09-13 09:14:16 +0800

    show the pet detail

--- src/main/webapp/scripts/angular-way.js
+++ src/main/webapp/scripts/angular-way.js
@@ -4,8 +4,14 @@ module.factory("petService", ["$http", function ($http) {
     function getAllPets() {
         return $http.get("/api/pets")
     }
+
+    function getPetById(id) {
+        return $http.get("/api/pets/" + id)
+    }
+
     return {
-        pets: getAllPets
+        pets: getAllPets,
+        petById: getPetById
     }
 }]);
 
@@ -15,12 +21,25 @@ module.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'pets.html',
             controller: 'petsCtrl'
         })
+        .when('/pets/:id', {
+            templateUrl: 'petDetail.html',
+            controller: 'petDetailCtrl'
+        })
         .otherwise({redirectTo: '/pets'});
 }]);
 
 module.controller('petsCtrl', ['$scope', 'petService', function ($scope, petService) {
     petService.pets().then(function (data) {
         $scope.pets = data.data;
+    }, function (error) {
+        return error.data;
+    });
+}]);
+
+module.controller('petDetailCtrl', ['$scope', '$routeParams', 'petService', function ($scope, $routeParams, petService) {
+    var petId = $routeParams.id;
+    petService.petById(petId).then(function (data) {
+        $scope.pet = data.data;
     }, function (error) {
         return error.data;
     });
